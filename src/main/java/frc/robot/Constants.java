b4f65b5c051ef89a1b1d88cb84c9c57f37ccded0
gmattana2 <giorgiamattana@hotmail.com>
2023-05-09 19:59:44 -0500

    setup stuff

--- src/main/java/frc/robot/Constants.java
+++ src/main/java/frc/robot/Constants.java
@@ -16,4 +16,42 @@ public final class Constants {
   public static class OperatorConstants {
     public static final int kDriverControllerPort = 0;
   }
+  public static class Buttons {
+    /**
+ * Gamepad Button List
+ */
+  public static final int LST_BTN_A = 1;
+  public static final int LST_BTN_B = 2;
+  public static final int LST_BTN_X = 3;
+  public static final int LST_BTN_Y = 4;
+  public static final int LST_BTN_LBUMPER = 5;
+  public static final int LST_BTN_RBUMPER = 6;
+  public static final int LST_BTN_WINDOW = 7;
+  public static final int LST_BTN_MENU = 8;
+  public static final int LST_BTN_LJOYSTICKPRESS = 9;
+  public static final int LST_BTN_RJOYSTICKPRESS = 10;
+
+/**
+ * Gamepad POV List
+ */
+  public static final int LST_POV_UNPRESSED = -1;
+  public static final int LST_POV_N = 0;
+  public static final int LST_POV_NE = 45;
+  public static final int LST_POV_E = 90;
+  public static final int LST_POV_SE = 135;
+  public static final int LST_POV_S = 180;
+  public static final int LST_POV_SW = 225;
+  public static final int LST_POV_W = 270;
+  public static final int LST_POV_NW = 315;
+
+/**
+ * Gamepad Axis List
+ */
+  public static final int LST_AXS_LJOYSTICKX = 0;
+  public static final int LST_AXS_LJOYSTICKY = 1;
+  public static final int LST_AXS_LTRIGGER = 2;
+  public static final int LST_AXS_RTRIGGER = 3;
+  public static final int LST_AXS_RJOYSTICKX = 4;
+  public static final int LST_AXS_RJOYSTICKY = 5;
+}
 }
