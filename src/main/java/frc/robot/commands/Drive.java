3c46997152967e20cedbb8722f6fe604d42ea469
error3130drive <giorgiamattana@hotmail.com>
2023-05-31 20:26:23 -0500

    test

---
+++ src/main/java/frc/robot/commands/Drive.java
@@ -0,0 +1,47 @@
+// Copyright (c) FIRST and other WPILib contributors.
+// Open Source Software; you can modify and/or share it under the terms of
+// the WPILib BSD license file in the root directory of this project.
+
+package frc.robot.commands;
+
+import frc.robot.Constants;
+import frc.robot.subsystems.Chassis;
+import edu.wpi.first.wpilibj2.command.CommandBase;
+import frc.robot.RobotContainer;
+
+public class Drive extends CommandBase {
+
+  private final Chassis m_chassis;
+  private final RobotContainer m_robotContainer;
+
+  public Drive(Chassis subsystem, RobotContainer container) {
+    m_chassis = subsystem;
+    m_robotContainer = container;
+
+    // Use addRequirements() here to declare subsystem dependencies.
+    addRequirements(subsystem);
+  }
+  //test lolsies
+
+  // Called when the command is initially scheduled.
+  @Override
+  public void initialize() {m_chassis.configRampRate(Constants.Chassis.kMaxRampRate);}
+
+  // Called every time the scheduler runs while the command is scheduled.
+  @Override
+  public void execute() {
+    double moveSpeed = -RobotContainer.m_driverGamepad.getRawAxis(1);
+    double turnSpeed = RobotContainer.m_driverGamepad.getRawAxis(4) * .8;
+    m_chassis.driveArcade(moveSpeed,turnSpeed,true);
+  }
+
+  // Called once the command ends or is interrupted.
+  @Override
+  public void end(boolean interrupted) {m_chassis.configRampRate(0);}
+
+  // Returns true when the command should end.
+  @Override
+  public boolean isFinished() {
+    return false;
+  }
+}
