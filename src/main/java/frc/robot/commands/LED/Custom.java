d121406f7b353836a97c8ba78102461df28ad783
Mohnish <95386817+MussMan67@users.noreply.github.com>
2024-02-07 21:02:03 -0600

    Added Custom

--- src/main/java/frc/robot/commands/LED/Custom.java
+++ src/main/java/frc/robot/commands/LED/Custom.java
@@ -2,15 +2,32 @@
 // Open Source Software; you can modify and/or share it under the terms of
 // the WPILib BSD license file in the root directory of this project.
 
+
 package frc.robot.commands.LED;
+
 
 import edu.wpi.first.wpilibj2.command.CommandBase;
 import frc.robot.subsystems.LEDSubsystem;
+
+
+
+// Copyright (c) FIRST and other WPILib contributors.
+// Open Source Software; you can modify and/or share it under the terms of
+// the WPILib BSD license file in the root directory of this project.
+
+
+package frc.robot.commands.LED;
+
+
+import edu.wpi.first.wpilibj2.command.CommandBase;
+import frc.robot.subsystems.LEDSubsystem;
+
 
 /** An example command that uses an example subsystem. */
 public class Custom extends CommandBase {
   @SuppressWarnings({"PMD.UnusedPrivateField", "PMD.SingularField"})
   private final LEDSubsystem ledSubsystem;
+
 
   /**
    * Creates a new ExampleCommand.
@@ -23,21 +40,34 @@ public class Custom extends CommandBase {
     addRequirements(ledSubsystem);
   }
 
+
   // Called when the command is initially scheduled.
   @Override
-  public void initialize() {}
+  public void initialize() {
+    if (!ledSubsystem.getLimitSwitch()) {
+      ledSubsystem.setCustom();
+    }
+  }
+
 
   // Called every time the scheduler runs while the command is scheduled.
   @Override
   public void execute() {
-    ledSubsystem.setCustom(ledSubsystem.getHue(), ledSubsystem.getSat(), ledSubsystem.getVal());
+    if (!ledSubsystem.getLimitSwitch()) {
+      ledSubsystem.setCustom();
+    }
+    else {
+      ledSubsystem.reset();
+    }
   }
+
 
   // Called once the command ends or is interrupted.
   @Override
   public void end(boolean interrupted) {
     ledSubsystem.reset();
   }
+
 
   // Returns true when the command should end.
   @Override
